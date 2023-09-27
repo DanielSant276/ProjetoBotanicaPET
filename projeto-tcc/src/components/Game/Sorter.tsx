3914e2fcfcde219eb9888834b85cd09b331e96e0
Daniel Sant' Anna Andrade <62079449+DanielSant276@users.noreply.github.com>
2023-09-27 08:44:19 -0300

    need to change

--- projeto-tcc/src/components/Game/Sorter.tsx
+++ projeto-tcc/src/components/Game/Sorter.tsx
@@ -2,31 +2,60 @@ import { Dispatch, SetStateAction, useEffect, useRef, useState } from 'react';
 import './Sorter.css';
 
 const totalNumbers = 21;
+const divWidth = [260]
 
 export function Sorter({ sendSortedNumber, startGame, setStartGame }: Props ) {
   const [sortedNumber, setSortedNumber] = useState<number>(-1);
   const [numbersSorted, setNumbersSorted] = useState<boolean[]>([]);
-  const leftoverNumbers = useRef<number[]>([]);
+  const [divDesmounted, setDivDesmounted] = useState<boolean[]>([]);
+  const [timesRotated, setTimesRotated] = useState<number>(0);
   
   const [disableButton, setDisableButton] = useState<boolean>(false);
-  const [moveLeft, setMoveLeft] = useState<boolean>(false);
   const [moveToLeft, setMoveToLeft] = useState<number>(0);
+  const [animationStoped, setAnimationStoped] = useState<boolean>(false);
+
+  const leftoverNumbers = useRef<number[]>([]);
+  const intervalId = useRef<NodeJS.Timer>();
 
   const generateAnimation = () => {
+    // if (sortedNumber !== -1) {
+    //   setNumbersSorted(numbersSorted.slice(0, 21));
+
+    //   for (let i = 0; i < totalNumbers; i++) {
+    //     divDesmounted.push(false);
+    //   }
+    // }
+
     setDisableButton(true);
-    setMoveLeft(true);
 
-    // Inicia o intervalo para mover a div a cada 100ms
-    const intervalId = setInterval(() => {
-      setMoveToLeft((prevPosition) => prevPosition + 80);
+    let index = Math.floor(Math.random() * leftoverNumbers.current.length);
+    let number = leftoverNumbers.current[index];
+    setSortedNumber(number);
+    console.log(number);
+
+    let rotation = Math.floor(Math.random() * 10) + 15;
+    setTimesRotated(rotation);
+    console.log(`número de voltas: ${rotation}`);
+
+    let speed = 500;
+
+    intervalId.current = setInterval(() => {
+      setMoveToLeft(prevPosition => prevPosition + speed);
+      speed -= 2;
+      // console.log(speed);
     }, 100);
+  }
 
-    // Define um timeout para parar o movimento após 2 segundos (por exemplo)
-    setTimeout(() => {
-      clearInterval(intervalId);
-      setMoveLeft(false);
-      setDisableButton(true);
-    }, 650 * 20);
+  useEffect(() => {
+    if (timesRotated > 0) {
+      console.log(`foram dadas: ${timesRotated}`);
+    }
+  }, [timesRotated]);
+
+  const stopAnimation = () => {
+    clearInterval(intervalId.current);
+    setDisableButton(false);
+    setTimesRotated(0);
   }
 
   // aqui é criado um número e depois marca ele como 'true' no numberSorted, tirando da lista leftoverNumbers
@@ -52,23 +81,11 @@ export function Sorter({ sendSortedNumber, startGame, setStartGame }: Props ) {
     }
   };
 
-  const positonImages = (index: number) => {
-    if (index === 0) {
-      return {
-        left: `calc(21px - ${moveToLeft}px)`,
-      }
-    }
-    else {
-      return {
-        left: `calc(21px + (var(--cardWidth) * ${index}) - ${moveToLeft}px)`,
-      }
-    }
-  }
-
   useEffect(() => {
     if (numbersSorted.length === 0) {
       for (let i = 0; i < totalNumbers; i++) {
         numbersSorted.push(false);
+        divDesmounted.push(false);
       }
       console.log(numbersSorted)
       console.log(numbersSorted.length)
@@ -90,16 +107,62 @@ export function Sorter({ sendSortedNumber, startGame, setStartGame }: Props ) {
       <div className='sorter-grid-intern'>
         {/* <p className='sorted-number not-selectable'> */}
           {startGame ? (
-            numbersSorted.map((item, index) => (
-              <div
-                key={index}
-                className='sorter-image-box align'
-                style={positonImages(index)}>
-                <p className={`sorted-number not-selectable ${item ? 'red-color' : 'white-color'}`}>
-                  {index}
-                </p>
-              </div>
-            ))
+            numbersSorted.map((item, index) => {              
+              let leftPosition;
+
+              if (animationStoped && index % totalNumbers === sortedNumber && Math.floor(index / totalNumbers) === timesRotated) {
+                leftPosition = 21;
+              }
+              else {
+                leftPosition = 21 - moveToLeft + (divWidth[0] * index);
+              }
+
+              if (leftPosition >= 1060) {
+                return null;
+              }
+
+              if (leftPosition <= -810) {
+                if (!divDesmounted[index]) {
+                  let divsStatus = divDesmounted;
+                  divsStatus[index] = true;
+                  divsStatus.push(false);
+                  setDivDesmounted(divsStatus);
+
+                  let newNumbersSorted = numbersSorted;
+                  newNumbersSorted.push(item);
+                  setNumbersSorted(newNumbersSorted);
+
+                  if (index % totalNumbers === 0) {
+                    setTimesRotated(prevState => prevState++)
+                  }
+                }
+                return null;
+              }
+
+              let value = index % totalNumbers;
+              if (leftPosition < 21 && index % totalNumbers === sortedNumber && Math.floor(index / totalNumbers) === timesRotated) {
+                setAnimationStoped(true);
+                stopAnimation();
+
+                return null;
+              }
+              else if (leftPosition !== 21 && index % totalNumbers !== sortedNumber && Math.floor(index / totalNumbers) >= timesRotated) {
+                return null;
+              }
+
+              let val = index % totalNumbers;
+
+              return (
+                <div
+                  key={index}
+                  className='sorter-image-box align'
+                  style={{left: `${leftPosition}px`}}>
+                  <p className={`sorted-number not-selectable ${item ? 'red-color' : 'white-color'}`}>
+                    {val}
+                  </p>
+                </div>
+              )
+            })
           ) : (
             <div>
               <p className='sorted-number'></p>
